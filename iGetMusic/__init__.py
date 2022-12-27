f5c66375d1b8e8a6bf14178bc8d6e7da5647a71f
Waradu <einfach_noan@outlook.de>
2022-12-27 20:56:06 +0100

    0.0.5

--- iGetMusic/__init__.py
+++ iGetMusic/__init__.py
@@ -1,4 +1,7 @@
-from .iGet import *
+from .iGet import resizeImage
+from .iGet import get
+from .iGet import getArtist
+from .iGet import getAllArtistSongs
 
 
-__all__ = ('get')
+__all__ = ('resizeImage', 'get', 'getArtist', 'getAllArtistSongs')
